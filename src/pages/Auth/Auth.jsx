564ee023e98ef9253bb8c5329cb69f5eefc9617a
brijraj <brijrajsinh.p@tridhyatech.com>
2023-06-12 14:11:15 +0530

    initialised the otp login

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import Hexagons from "../../components/Hexagons/Hexagons";
+import animation1 from "../../assets/Lottie/animation1.json"
+import animation2 from "../../assets/Lottie/animation2.json"
+import Lottie from "lottie-react";
 import "./Auth.css";
 
 export default function Auth() {
   const [isLogin, setIsLogin] = useState(true);
+  const [codeLogin,setCodeLogin] = useState(false);
   const toogleLogin = () => {
     setIsLogin((prev) => !prev);
   };
+  const toogleCodelogin = ()=>{
+    setCodeLogin(prev=>!prev);
+  }
   return (
     <div className="auth">
       <Hexagons />
       <div className="auth_contents">
         <div className="left">
           <h1>LinkVaut</h1>
+          <div className="anim_contain">
+            <Lottie animationData={isLogin?animation1:animation2} style={{width: "100%"}}/>
+          </div>
         </div>
         <div className="right">
+        <h1 className="right_branding">LinkVaut</h1>
           <div className="login_card">
             <div className="card_braces">
               <div className="card_header">
@@ -26,11 +37,14 @@ export default function Auth() {
                 </p>
               </div>
 
-              <form action="" className="auth_form">
+              {!codeLogin && <form action="" className="auth_form">
                 <p>Email address</p>
                 <input type="text" className="inputfield" />
                 <p>Password</p>
                 <input type="password" className="inputfield" />
+                <p className="access_code">
+                  use <span className="toogle_login" onClick={toogleCodelogin}>Access code</span>
+                </p>
                 <input
                   type="submit"
                   value={isLogin ? "Sign in" : "Register"}
@@ -42,7 +56,12 @@ export default function Auth() {
                     {isLogin ? "Create Account" : "Signin"}
                   </p>
                 </div>
-              </form>
+              </form>}
+
+              <div className="code_input_contain">
+                
+              </div>
+
             </div>
           </div>
         </div>
