54c16889d5f48250dc34e213fccfa22a605f03e6
brijraj <brijrajsinh.p@tridhyatech.com>
2023-07-17 17:33:57 +0530

    see only notes created by user

--- src/Hooks/useGetLinks.js
+++ src/Hooks/useGetLinks.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import API from "../Axios/axios";
+import useAuthContext from "./ContextHooks/useAuthContext";
 
 const useGetLinks = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const {user} = useAuthContext();
   const getLinks = async () => {
     setLoading(true);
     console.log("fetching data");
-    const { data } = await API.get("/links");
+    const { data } = await API.get(`/links/${user._id}`);
     setLoading(false);
     setError(null);
     return data;
--- src/Hooks/usePostLinks.js
+++ src/Hooks/usePostLinks.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import API from "../Axios/axios";
+import useAuthContext from "./ContextHooks/useAuthContext";
 
 const usePostLinks = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { user } = useAuthContext();
   const saveLinks = async (newLinks) => {
     setLoading(true);
     const { data } = await API.post("/links", {
       data: JSON.stringify(newLinks),
+      user:user._id,
     });
     setError(false);
     setLoading(false);
