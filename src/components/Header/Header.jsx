36ab40f2802eab0332967c03773c7afaf948e657
Brijrajsinh Parmar <brijrajparmar27@gmail.com>
2023-07-27 14:50:12 +0530

    added drawer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,50 @@
-import "./Header.css"
+import "./Header.css";
 import useAuthContext from "../../Hooks/ContextHooks/useAuthContext";
 import { BsViewList, BsPlusLg, BsGridFill } from "react-icons/bs";
+import { useDisclosure } from "@mantine/hooks";
+import { Box, Center, Drawer, Group, SegmentedControl } from "@mantine/core";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = ({ setIsList, setLinkModalState, isList }) => {
   const { logout } = useAuthContext();
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <div className="home_header">
-      <h1 onClick={logout}>LinkVault</h1>
-      <div className="menus">
-        <div
-          className="switcher_contain"
-          onClick={() => {
-            setIsList((prev) => !prev);
-          }}
-        >
-          {isList ? (
-            <BsViewList className="view_switcher" />
-          ) : (
-            <BsGridFill className="view_switcher" />
-          )}
+      <AiOutlineMenu onClick={open} className="view_switcher" />
+      <Drawer opened={opened} onClose={close} title="Menu" size={"xs"}>
+        <div className="switch_contain">
+          <SegmentedControl
+            value={isList ? "List" : "Grid"}
+            onChange={(value) => {
+              setIsList(value === "List" ? true : false);
+            }}
+            size="lg"
+            data={[
+              {
+                label: (
+                  <div className="switcher_toogle">
+                    <BsViewList className="view_switcher" /> <p>List</p>
+                  </div>
+                ),
+                value: "List",
+              },
+              {
+                label: (
+                  <div className="switcher_toogle">
+                    <BsGridFill className="view_switcher" /> <p>Grid</p>
+                  </div>
+                ),
+                value: "Grid",
+              },
+            ]}
+          />
         </div>
+        <button className="logout_btn" onClick={logout}>
+          Logout
+        </button>
+      </Drawer>
+      <h1>LinkVault</h1>
+      <div className="menus">
         <button
           className="addLink_btn"
           onClick={() => {
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,20 @@ import { BrowserRouter } from "react-router-dom";
 import { LinkProvider } from "./contexts/LinkContext.jsx";
 import { ModalProvider } from "./contexts/ModalContext.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <LinkProvider>
       <ModalProvider>
         <AuthProvider>
-          <App />
+          <MantineProvider
+            theme={{ fontFamily: "Open Sans" }}
+            withGlobalStyles
+            withNormalizeCSS
+          >
+            <App />
+          </MantineProvider>
         </AuthProvider>
       </ModalProvider>
     </LinkProvider>
