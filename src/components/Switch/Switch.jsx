c64b99e035b210b37872e82fee7a772e9bef54af
Brijrajsinh Parmar <brijrajparmar27@gmail.com>
2023-07-31 11:17:14 +0530

    repo assets

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -2,33 +2,33 @@ import { SegmentedControl } from "@mantine/core";
 import { BsViewList, BsGridFill } from "react-icons/bs";
 
 export const Switch = ({ isList, setIsList, close }) => {
-  const controls = [
-    {
-      title: "List",
-      icon: <BsViewList className="view_switcher" />,
-    },
-    {
-      title: "Grid",
-      icon: <BsGridFill className="view_switcher" />,
-    },
-  ];
-  const composeSwitchItems = () => {
-    // let items = [];
-    let items = controls.map((each) => {
-      console.log(each);
-      return {
-        label: (
-          <div className="switcher_toogle">
-            {each.icon}
-            {each.title}
-          </div>
-        ),
-        value: each.title,
-      };
-    });
-    console.log(items);
-  };
-  composeSwitchItems();
+//   const controls = [
+//     {
+//       title: "List",
+//       icon: <BsViewList className="view_switcher" />,
+//     },
+//     {
+//       title: "Grid",
+//       icon: <BsGridFill className="view_switcher" />,
+//     },
+//   ];
+//   const composeSwitchItems = () => {
+//     // let items = [];
+//     let items = controls.map((each) => {
+//       console.log(each);
+//       return {
+//         label: (
+//           <div className="switcher_toogle">
+//             {each.icon}
+//             {each.title}
+//           </div>
+//         ),
+//         value: each.title,
+//       };
+//     });
+//     console.log(items);
+//   };
+//   composeSwitchItems();
   const handleSwitchToogle = (value) => {
     setIsList(value === "List" ? true : false);
     setTimeout(() => {
